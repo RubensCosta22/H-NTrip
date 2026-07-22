44c605a93cc2311d04c79dd29bc91e8a462e61e9
RubensCosta22 <97482359+RubensCosta22@users.noreply.github.com>
2026-07-22 17:31:44 -0300

    Load H&NTrip auth brand layer

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import { AuthShell } from "@/src/features/access/auth-shell";
 import "./auth.css";
+import "./auth-brand.css";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return <AuthShell>{children}</AuthShell>;
